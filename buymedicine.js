e44e3c611429a93b5af2967b29beb23c0325d3bd
Kushal Kumar Banyal <101243584+kushalkumar09@users.noreply.github.com>
2025-04-18 00:28:49 +0530

    added cart page with functionalities

--- buymedicine.js
+++ buymedicine.js
@@ -206,6 +206,7 @@ document.addEventListener("DOMContentLoaded", function () {
   function getCart() {
     return JSON.parse(localStorage.getItem("cart")) || [];
   }
+  
 
   function saveCart(cart) {
     localStorage.setItem("cart", JSON.stringify(cart));
